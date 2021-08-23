ed22d4867a5d1a27aad0e3a7d4446cfea9f6ffeb
Alexandre Dumont <alexandre.dumont3@usherbrooke.ca>
2021-08-23 09:32:59 -0400

    Updated python module and cleaned makefiles

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 make -C FFT/
 make -C Correlations/
 make -C Math/
+make -C Histograms/
 
 mv FFT/obj/libfft.* SignalProcessing/fft/.
 mv Math/obj/libmath.* SignalProcessing/math/.
 mv Correlations/obj/libcorrelations.* SignalProcessing/correlations/.
+mv Histograms/obj/libhistograms.* SignalProcessing/histograms/.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="SignalProcessing",
-    version="1.2",
+    version="1.3",
     author="Alexandre Dumont",
     author_email="Alexandre.Dumont3@usherbrooke.ca",
     long_description=long_description,
