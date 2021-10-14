0b17e94ee4f6c9455b746c38885c5c3aac067fad
Alexandre Dumont <alexandre.dumont3@usherbrooke.ca>
2021-10-14 10:18:10 -0400

    Updated version of setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="SignalProcessing",
-    version="1.3.1",
+    version="1.4",
     author="Alexandre Dumont",
     author_email="Alexandre.Dumont3@usherbrooke.ca",
     long_description=long_description,
