bd19431ad77b5134c2b02c27b5d71018ace896fd
Alexandre Dumont <56271618+a-dumont@users.noreply.github.com>
2022-06-21 16:23:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['numpy','pybind11'],
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False
