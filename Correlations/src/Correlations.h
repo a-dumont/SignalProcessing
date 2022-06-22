a89667cb2b7ead390366e00b5c89a53a42cd4e29
Alexandre Dumont <alexandre.dumont3@usherbrooke.ca>
2022-06-22 11:39:25 -0400

    Added better templating, but have not implemented float yet.

--- Correlations/src/Correlations.h
+++ Correlations/src/Correlations.h
@@ -2,6 +2,7 @@
 #include <fftw3.h>
 #include <string>
 #include <stdio.h>
+#include <complex>
 
 #include "../../FFT/src/FFT.h"
 #include "Correlations.tpp"
--- Correlations/src/Correlations_py.cpp
+++ Correlations/src/Correlations_py.cpp
@@ -1,15 +1,13 @@
 #include "Correlations_py.h"
-#include <complex>
-#include <type_traits>
 
 void init_correlations(py::module &m)
 {
-	m.def("auto_correlation",&autocorrelation_py<np_double>, "In"_a, "Given only 1 argument, will compute the full length auto-correlation");
-	m.def("auto_correlation",&autocorrelation_py<np_double,int>, "In"_a, "N"_a, "Will compute the length N auto-correlation");
-	m.def("cross_correlation",&xcorrelation_py<np_double>, "In1"_a, "In2"_a,"Will compute the full length cross-correlation");
-	m.def("cross_correlation",&xcorrelation_py<np_double,int>, "In1"_a, "In2"_a, "N"_a, "Will compute the length N cross-correlation");
-	m.def("complete_correlation",&complete_correlation_py<np_double>, "In1"_a, "In2"_a,"Will compute all 3 full length correlations");
-	m.def("complete_correlation",&complete_correlation_py<np_double,int>, "In1"_a, "In2"_a, "N"_a, "Will compute all 3 length N correlations");
+	m.def("auto_correlation",&autocorrelation_py<double>, "In"_a, "Given only 1 argument, will compute the full length auto-correlation");
+	m.def("auto_correlation",&autocorrelation_py<double,int>, "In"_a, "N"_a, "Will compute the length N auto-correlation");
+	m.def("cross_correlation",&xcorrelation_py<double>, "In1"_a, "In2"_a,"Will compute the full length cross-correlation");
+	m.def("cross_correlation",&xcorrelation_py<double,int>, "In1"_a, "In2"_a, "N"_a, "Will compute the length N cross-correlation");
+	m.def("complete_correlation",&complete_correlation_py<double>, "In1"_a, "In2"_a,"Will compute all 3 full length correlations");
+	m.def("complete_correlation",&complete_correlation_py<double,int>, "In1"_a, "In2"_a, "N"_a, "Will compute all 3 length N correlations");
 }
 
 PYBIND11_MODULE(libcorrelations, m)
