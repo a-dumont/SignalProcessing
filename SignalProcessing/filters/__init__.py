676a6468c6bc58ecc7bb36d6b38c9c0d477ac47c
Alexandre Dumont <alexandre.dumont3@usherbrooke.ca>
2023-08-02 13:45:52 -0400

    Added Filters

---
+++ SignalProcessing/filters/__init__.py
@@ -0,0 +1,19 @@
+import os
+s = os.path.abspath("C:/cygwin64/usr/x86_64-w64-mingw32/sys-root/mingw/bin")
+
+if os.name == "nt" and s not in os.environ["PATH"]:
+  os.environ["PATH"] = s+";"+os.environ["PATH"]
+
+from .libfilters import *
+
+#try:
+#    from .libfilterscuda import *
+#    del libfilterscuda
+#except ImportError:
+#        print("No CUDA support")
+#except ModuleNotFoundError:
+#        print("No CUDA support")
+
+del s
+del os
+del libfilters
