83fc5e8ad4316f0c0710fafa9743c5fe9fc90628
Alexandre Dumont <56271618+a-dumont@users.noreply.github.com>
2023-12-21 19:22:39 -0500

    Added axCorrAVX

--- Correlations_NEW/src/Correlations_py.cpp
+++ Correlations_NEW/src/Correlations_py.cpp
@@ -2,16 +2,23 @@
 
 void init_correlations(py::module &m)
 {
-	// aCorr
+	// Acorr
 	m.def("aCorrCircularFreqAVX",&aCorrCircularFreqAVX_py<float>, "In"_a.noconvert(),"size"_a);
 	m.def("aCorrCircularFreqAVX",&aCorrCircularFreqAVX_py<double>, "In"_a.noconvert(),"size"_a);
 	
-	// xCorr
+	// Xcorr
 	m.def("xCorrCircularFreqAVX",&xCorrCircularFreqAVX_py<float>, 
 					"In1"_a.noconvert(), "In2"_a.noconvert(), "size"_a);
 	m.def("xCorrCircularFreqAVX",&xCorrCircularFreqAVX_py<double>, 
 					"In1"_a.noconvert(), "In2"_a.noconvert(), "size"_a);
+					
+	// Combined Acorr and Xcorr
+	m.def("axCorrCircularFreqAVX",&axCorrCircularFreqAVX_py<float>, 
+					"In1"_a.noconvert(), "In2"_a.noconvert(), "size"_a);
+	m.def("axCorrCircularFreqAVX",&axCorrCircularFreqAVX_py<double>, 
+					"In1"_a.noconvert(), "In2"_a.noconvert(), "size"_a);
 	
+	// Reduction
 	m.def("reduceAVX",&reduceAVX_py<float>, "In"_a.noconvert());
 	m.def("reduceAVX",&reduceAVX_py<double>, "In"_a.noconvert());
 	
@@ -21,6 +28,6 @@ void init_correlations(py::module &m)
 
 PYBIND11_MODULE(libcorrelations, m)
 {
-	m.doc() = "Contains correlation methods using FFTs";
+	m.doc() = "Contains correlation methods.";
 	init_correlations(m);
 }
