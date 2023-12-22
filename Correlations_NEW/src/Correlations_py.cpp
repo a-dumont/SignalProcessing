e463d9dd88a04ad4efa3319c0054f78b4bffadec
Alexandre Dumont <56271618+a-dumont@users.noreply.github.com>
2023-12-22 11:00:47 -0500

    Started working on ACorrCircularFreqAVX

--- Correlations_NEW/src/Correlations_py.cpp
+++ Correlations_NEW/src/Correlations_py.cpp
@@ -6,6 +6,17 @@ void init_correlations(py::module &m)
 	m.def("aCorrCircularFreqAVX",&aCorrCircularFreqAVX_py<float>, "In"_a.noconvert(),"size"_a);
 	m.def("aCorrCircularFreqAVX",&aCorrCircularFreqAVX_py<double>, "In"_a.noconvert(),"size"_a);
 	
+	py::class_<ACorrCircularFreqAVX_py>(m,"ACorrCircularFreqAVX")
+			.def(py::init<uint64_t,uint64_t>())
+			.def("aCorrCircularFreqAVX",&ACorrCircularFreqAVX_py::aCorrCircularFreqAVX,
+				"In"_a.noconvert())
+			//.def("rfftBlock",&RFFT_Block_py::rfftBlockf,"In"_a.noconvert())
+			.def("getSize",&ACorrCircularFreqAVX_py::getSize)
+			.def("getN",&ACorrCircularFreqAVX_py::getN)
+			.def("getHowmany",&ACorrCircularFreqAVX_py::getHowmany)
+			.def("benchmark",&ACorrCircularFreqAVX_py::benchmark)
+			.def("train",&ACorrCircularFreqAVX_py::train);
+	
 	// Xcorr
 	m.def("xCorrCircularFreqAVX",&xCorrCircularFreqAVX_py<float>, 
 					"In1"_a.noconvert(), "In2"_a.noconvert(), "size"_a);
