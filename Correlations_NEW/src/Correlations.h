c10564c43c7bf317c0037e877009e8b170869d1e
Alexandre Dumont <56271618+a-dumont@users.noreply.github.com>
2023-12-21 19:20:59 -0500

    Added info to .h files.

--- Correlations_NEW/src/Correlations.h
+++ Correlations_NEW/src/Correlations.h
@@ -13,15 +13,35 @@
 
 // Acorr
 template<class DataType>
-void aCorrCircularFreqAVX(uint64_t N, uint64_t size, DataType* in, DataType* out);
+void aCorrCircularFreqAVX(uint64_t N, DataType* in, DataType* out);
 
-// Others
+// Xcorr
+template<class DataType>
+void xCorrCircularFreqAVX(uint64_t N, DataType* in1, DataType* in2, DataType* out);
+
+// Combined Acorr and Xcorr
+template<class DataType>
+void axCorrCircularFreqAVX(uint64_t N, DataType* in1, DataType* in2, 
+				DataType* out1, DataType* out2, DataType* out3);
+
+// rFFT
+template<class DataType>
+void rfftBlock(int N, int size, DataType* in, std::complex<DataType>* out);
+
+// Type conversion
 template<class DataTypeIn, class DataTypeOut>
 void convertAVX(uint64_t N, DataTypeIn* in, DataTypeOut* out);
 
 template<class DataTypeIn, class DataTypeOut>
 void convertAVX_pad(uint64_t N, uint64_t Npad,
 				DataTypeIn* in, DataTypeOut* out, DataTypeOut conv, DataTypeIn offset);
+				
+// Reduction
+template<class DataType>
+void reduceAVX(uint64_t N, DataType* in, DataType* out);
+
+template<class DataType>
+void reduceBlockAVX(uint64_t N, uint64_t size, DataType* in, DataType* out);
 
 // .tpp template definitions 
 #include "Correlations.tpp"
--- Correlations_NEW/src/Correlations_py.h
+++ Correlations_NEW/src/Correlations_py.h
@@ -9,8 +9,32 @@ using namespace pybind11::literals;
 
 #include "Correlations.h"
 
+// Acorr
+template<class DataType>
+py::array_t<DataType,py::array::c_style>
+aCorrCircularFreqAVX_py(py::array_t<DataType,py::array::c_style> py_in, uint64_t size);
 
+// Xcorr
+template<class DataType>
+py::array_t<std::complex<DataType>,py::array::c_style>
+xCorrCircularFreqAVX_py(py::array_t<DataType,py::array::c_style> py_in1, 
+				py::array_t<DataType,py::array::c_style> py_in2, uint64_t size);
 
+// Combined Acorr and Xcorr
+template<class DataType>
+std::tuple<py::array_t<DataType,py::array::c_style>,
+py::array_t<DataType,py::array::c_style>,
+py::array_t<std::complex<DataType>,py::array::c_style>>
+axCorrCircularFreqAVX_py(py::array_t<DataType,py::array::c_style> py_in1, 
+				py::array_t<DataType,py::array::c_style> py_in2, uint64_t size);
+
+// Reduction
+template<class DataType>
+DataType reduceAVX_py(py::array_t<DataType,py::array::c_style> py_in);
+
+template<class DataType>
+py::array_t<DataType,py::array::c_style>
+reduceBlockAVX_py(py::array_t<DataType,py::array::c_style> py_in, uint64_t size);
  
 // .tpp definitions
 #include "Correlations_py.tpp"
