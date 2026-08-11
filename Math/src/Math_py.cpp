4e90e2e661d3791094fdc5c659692fd8f7973dc7
Alexandre Dumont <alexandre.dumont3@usherbrooke.ca>
2026-08-11 14:35:53 -0400

    Math refactor

--- Math/src/Math_py.cpp
+++ Math/src/Math_py.cpp
@@ -5,16 +5,22 @@ void init_module(py::module &m)
 	//Fullsize gradient
 	m.def("gradient", &gradient_py<double,double>, "x"_a, "t"_a.noconvert());
 	m.def("gradient", &gradient_py<double,float>, "x"_a, "t"_a.noconvert());
-	m.def("gradient", &gradient_py<double,long long int>, "x"_a, "t"_a.noconvert());
+	m.def("gradient", &gradient_py<double,int64_t>, "x"_a, "t"_a.noconvert());
+	m.def("gradient", &gradient_py<double,uint64_t>, "x"_a, "t"_a.noconvert());
 	m.def("gradient", &gradient_py<float,double>, "x"_a, "t"_a.noconvert());
 	m.def("gradient", &gradient_py<float,float>, "x"_a, "t"_a.noconvert());
-	m.def("gradient", &gradient_py<float,long long int>, "x"_a, "t"_a.noconvert());
+	m.def("gradient", &gradient_py<float,int64_t>, "x"_a, "t"_a.noconvert());
+	m.def("gradient", &gradient_py<float,uint64_t>, "x"_a, "t"_a.noconvert());
 	
 	//Gradient with constant step
 	m.def("gradient", &gradient2_py<double,double>, "x"_a, "dt"_a.noconvert());
-	m.def("gradient", &gradient2_py<double,long long int>, "x"_a, "dt"_a.noconvert());
+	m.def("gradient", &gradient2_py<double,float>, "x"_a, "dt"_a.noconvert());
+	m.def("gradient", &gradient2_py<double,int64_t>, "x"_a, "dt"_a.noconvert());
+	m.def("gradient", &gradient2_py<double,uint64_t>, "x"_a, "dt"_a.noconvert());
 	m.def("gradient", &gradient2_py<float,double>, "x"_a, "dt"_a.noconvert());
-	m.def("gradient", &gradient2_py<float,long long int>, "x"_a, "dt"_a.noconvert());
+	m.def("gradient", &gradient2_py<float,float>, "x"_a, "dt"_a.noconvert());
+	m.def("gradient", &gradient2_py<float,int64_t>, "x"_a, "dt"_a.noconvert());
+	m.def("gradient", &gradient2_py<float,uint64_t>, "x"_a, "dt"_a.noconvert());
 
 	//Finite difference for any reasonable symmetrical stencil
 	m.def("finite_difference_coefficients",&finite_difference_coefficients_py<double>,"M"_a,"N"_a);
@@ -22,31 +28,35 @@ void init_module(py::module &m)
 	
 	m.def("nth_order_gradient",&nth_order_gradient_py<double,double>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
 	m.def("nth_order_gradient",&nth_order_gradient_py<double,float>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
-	m.def("nth_order_gradient",&nth_order_gradient_py<double,long long int>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
+	m.def("nth_order_gradient",&nth_order_gradient_py<double,int64_t>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
+	m.def("nth_order_gradient",&nth_order_gradient_py<double,uint64_t>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
 	m.def("nth_order_gradient",&nth_order_gradient_py<float,double>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
 	m.def("nth_order_gradient",&nth_order_gradient_py<float,float>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
-	m.def("nth_order_gradient",&nth_order_gradient_py<float,long long int>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
+	m.def("nth_order_gradient",&nth_order_gradient_py<float,int64_t>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
+	m.def("nth_order_gradient",&nth_order_gradient_py<float,uint64_t>,"x"_a.noconvert(),"dt"_a,"M"_a,"N"_a);
 	
 	//Continuous maximum
 	m.def("continuous_max",&continuous_max_py<double>,"in"_a);
 	m.def("continuous_max",&continuous_max_py<float>,"in"_a);
-	m.def("continuous_max",&continuous_max_py<long long int>,"in"_a);
+	m.def("continuous_max",&continuous_max_py<int64_t>,"in"_a);
+	m.def("continuous_max",&continuous_max_py<uint64_t>,"in"_a);
 	
 	//Continuous minimum
 	m.def("continuous_min",&continuous_min_py<double>,"in"_a);
 	m.def("continuous_min",&continuous_min_py<float>,"in"_a);
-	m.def("continuous_min",&continuous_min_py<long long int>,"in"_a);
+	m.def("continuous_min",&continuous_min_py<int64_t>,"in"_a);
+	m.def("continuous_min",&continuous_min_py<uint64_t>,"in"_a);
 	
 	//Reduction of a vector to a scalar
 	m.def("Sum",&sum_py<double>,"in"_a);
 	m.def("Sum",&sum_py<float>,"in"_a);
-	m.def("Sum",&sum_py<long long int>,"in"_a);
+	m.def("Sum",&sum_py<uint64_t>,"in"_a);
 	m.def("Sum",&sum_py<dbl_complex>,"in"_a);
 	
 	//Reduction of a vector into a scalar mean
 	m.def("mean",&mean_py<double>,"in"_a);
 	m.def("mean",&mean_py<float>,"in"_a);
-	m.def("mean",&mean_py<long long int>,"in"_a);
+	m.def("mean",&mean_py<uint64_t>,"in"_a);
 	m.def("mean",&mean_complex_py<dbl_complex>,"in"_a);
 	
 	//Variance of a vector
@@ -60,39 +70,45 @@ void init_module(py::module &m)
 	//Product elementwise of two vectors
 	m.def("product",&product_py<double,double>,"in1"_a,"in2"_a);
 	m.def("product",&product_py<float,float>,"in1"_a,"in2"_a);
-	m.def("product",&product_py<long long int,long long int>,"in1"_a,"in2"_a);
+	m.def("product",&product_py<int64_t,int64_t>,"in1"_a,"in2"_a);
+	m.def("product",&product_py<uint64_t,uint64_t>,"in1"_a,"in2"_a);
 	m.def("product",&product_py<dbl_complex,dbl_complex>,"in1"_a,"in2"_a);
 	
 	//Sum elementwise of two vectors
 	m.def("Sum",&sum_py<double,double>,"in1"_a,"in2"_a);
 	m.def("Sum",&sum_py<float,float>,"in1"_a,"in2"_a);
-	m.def("Sum",&sum_py<long long int,long long int>,"in1"_a,"in2"_a);
+	m.def("Sum",&sum_py<int64_t,int64_t>,"in1"_a,"in2"_a);
+	m.def("Sum",&sum_py<uint64_t,uint64_t>,"in1"_a,"in2"_a);
 	m.def("Sum",&sum_py<dbl_complex,dbl_complex>,"in1"_a,"in2"_a);
 	
 	//Difference elementwise of two vectors
 	m.def("difference",&difference_py<double,double>,"in1"_a,"in2"_a);
 	m.def("difference",&difference_py<float,float>,"in1"_a,"in2"_a);
-	m.def("difference",&difference_py<long long int,long long int>,"in1"_a,"in2"_a);
+	m.def("difference",&difference_py<int64_t,int64_t>,"in1"_a,"in2"_a);
+	m.def("difference",&difference_py<uint64_t,uint64_t>,"in1"_a,"in2"_a);
 	m.def("difference",&difference_py<dbl_complex,dbl_complex>,"in1"_a,"in2"_a);
 	
 	//Division elementwise of two vectors
 	m.def("division",&division_py<double,double>,"in1"_a,"in2"_a);
 	m.def("division",&division_py<float,float>,"in1"_a,"in2"_a);
-	m.def("division",&division_py<long long int,long long int>,"in1"_a,"in2"_a);
+	m.def("division",&division_py<int64_t,int64_t>,"in1"_a,"in2"_a);
+	m.def("division",&division_py<uint64_t,uint64_t>,"in1"_a,"in2"_a);
 	m.def("division",&division_py<dbl_complex,dbl_complex>,"in1"_a,"in2"_a);
 	
 	//Minimum in a vector
 	m.def("minimum",&min_py<double>,"in"_a);
 	m.def("minimum",&min_py<float>,"in"_a);
-	m.def("minimum",&min_py<long long int>,"in"_a);
+	m.def("minimum",&min_py<int64_t>,"in"_a);
+	m.def("minimum",&min_py<uint64_t>,"in"_a);
 	
 	//Maximum in a vector
 	m.def("maximum",&max_py<double>,"in"_a);
 	m.def("maximum",&max_py<float>,"in"_a);
-	m.def("maximum",&max_py<long long int>,"in"_a);
+	m.def("maximum",&max_py<int64_t>,"in"_a);
+	m.def("maximum",&max_py<uint64_t>,"in"_a);
 
 	py::class_<DigitizerBlockMaxPy>(m,"DigitizerBlockMax")
-			.def(py::init<int64_t,int64_t,int64_t,int64_t>())
+			.def(py::init<uint64_t,uint64_t,uint64_t,uint64_t>())
 			.def("accumulate",&DigitizerBlockMaxPy::accumulate_py<uint8_t>)
 			.def("accumulate",&DigitizerBlockMaxPy::accumulate_py<uint16_t>)
 			.def("accumulate",&DigitizerBlockMaxPy::accumulate_py<int16_t>)
@@ -105,7 +121,7 @@ void init_module(py::module &m)
 			.def("clear",&DigitizerBlockMaxPy::clear);
 
 	py::class_<DigitizerBlockMinPy>(m,"DigitizerBlockMin")
-			.def(py::init<int64_t,int64_t,int64_t,int64_t>())
+			.def(py::init<uint64_t,uint64_t,uint64_t,uint64_t>())
 			.def("accumulate",&DigitizerBlockMinPy::accumulate_py<uint8_t>)
 			.def("accumulate",&DigitizerBlockMinPy::accumulate_py<uint16_t>)
 			.def("accumulate",&DigitizerBlockMinPy::accumulate_py<int16_t>)
@@ -118,7 +134,7 @@ void init_module(py::module &m)
 			.def("clear",&DigitizerBlockMinPy::clear);
 
 	py::class_<DigitizerBlockMinMaxPy>(m,"DigitizerBlockMinMax")
-			.def(py::init<int64_t,int64_t,int64_t,int64_t>())
+			.def(py::init<uint64_t,uint64_t,uint64_t,uint64_t>())
 			.def("accumulate",&DigitizerBlockMinMaxPy::accumulate_py<uint8_t>)
 			.def("accumulate",&DigitizerBlockMinMaxPy::accumulate_py<uint16_t>)
 			.def("accumulate",&DigitizerBlockMinMaxPy::accumulate_py<int16_t>)
